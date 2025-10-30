a9cdff28325ced09ae4e1f6210e48542d785bf21
벨라 <icomksh@gmail.com>
2025-10-30 11:43:24 +0900

    [FEAT/#34] 환자 엔티티 설정

--- app/db_models/patient.py
+++ app/db_models/patient.py
@@ -1,20 +1,11 @@
-from sqlalchemy import Column, Integer, String, Date, Enum
+from sqlalchemy import Column, Integer, String
 from app.core.db import Base
 from app.db_models.base_entity import TimestampMixin
 
 
-# 사용자 테이블 정의
-
-GenderEnum = Enum("MALE", "FEMALE", name="gender_enum")
-UserStatus = Enum("ACTIVE", "INACTIVE", name="status_enum")
-RoleEnum = Enum("USER", "ADMIN", name="role_enum")
-
-class Users(Base, TimestampMixin):
-    __tablename__ = "users"
+# 환자 테이블 정의
+class Patient(Base, TimestampMixin):
+    __tablename__ = "patient"
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(20), nullable=False) # 이름
-    phoneNum = Column(String(11), nullable=False) # 휴대폰번호
-    birth = Column(Date, nullable=False) # 생년월일
-    gender = Column(GenderEnum, nullable = False) # 성별
-    status = Column(UserStatus, nullable=False) # 활성화 여부
-    role = Column(RoleEnum, nullable=False) # 권한
+    loginId = Column(String(50), nullable=False) # 환자 아이디 (로그인용)
+    password = Column(String(100), nullable=False) # 환자 비밀번호 (로그인용)
